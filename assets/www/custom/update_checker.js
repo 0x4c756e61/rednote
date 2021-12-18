cfb0d8a8777fb2737bf6cd2651ed2f680f3ab786
0x454d505459 <40660493+0x454d505459@users.noreply.github.com>
2021-12-18 18:25:57 +0100

    Added a more complex update checker

--- assets/www/custom/update_checker.js
+++ assets/www/custom/update_checker.js
@@ -1,9 +1,10 @@
 const current_ver = "v3"
 
-fetch('https://raw.githubusercontent.com/0x454d505459/rednote/main/version.info')
+fetch('https://github.com/0x454d505459/rednote/releases')
 .then(r => r.text())
-.then( version => {
-    if (version !== current_ver) {
-        alert("New update is available on github!")
+.then(text => {
+    let latest_ver = text.match(/<span class="ml-1 wb-break-all">\n*\s*v\d/g)[0].replace('<span class="ml-1 wb-break-all">', '').replace(/\s||\n/g, '')
+    if (latest_ver != current_ver) {
+        alert(`Update ${latest_ver} is now out on github!\nYour current version: ${current_ver}`)
     }
 })
